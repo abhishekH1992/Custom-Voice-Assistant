8ff2a5765b2d82fc06c5bc5eee1fcc3dc0e9d689
Abhishek Honrao <abhishek@limesharp.net>
2024-09-13 16:25:48 +1200

    name added

--- server/migrations/20240913042049-create-saved-chats.js
+++ server/migrations/20240913042049-create-saved-chats.js
@@ -9,6 +9,10 @@ module.exports = {
                 primaryKey: true,
                 type: Sequelize.INTEGER
             },
+            name: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
             userId: {
                 type: Sequelize.INTEGER,
                 allowNull: false,
