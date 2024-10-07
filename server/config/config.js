10a4a614ad92c9cf876356c91333a220ed13788d
Abhishek Honrao <abhishek@limesharp.net>
2024-10-07 19:46:47 +1300

    DB chnaged

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ module.exports = {
         "dialect": "mysql"
     },
     "production": {
-        "use_env_variable": "mysql://akoplusco_node_user:gb0mxf0(jZ6w@node56.myfcloudau.com:3306/akoplusco_node",
+        "use_env_variable": "mysql://akoplusco_convo:vOm)RMjzT,Uj@node56.myfcloudau.com:3306/akoplusco_convo",
         "dialect": "mysql",
         "dialectOptions": {
             "ssl": {
