9dabdb2ed92d912717c54f309605caa236fd857f
abhishekH1992 <abhishekhonrao103@gmail.com>
2024-10-20 10:58:15 +1300

    Frontend issue

--- server/index.js
+++ server/index.js
@@ -92,6 +92,16 @@ async function startServer() {
         })
     );
 
+    if (process.env.NODE_ENV === 'production') {
+        // Serve static files from the React app
+        app.use(express.static(path.join(__dirname, 'client/build')));
+    
+        // Handles any requests that don't match the ones above
+        app.get('*', (req, res) => {
+            res.sendFile(path.join(__dirname + '/client/build/index.html'));
+        });
+    }
+
     const PORT = process.env.PORT || 5000;
 
     httpServer.listen(PORT, () => {
