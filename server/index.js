cc8d9fec186a62432cf9f2a4d8bf1276f3b0fb15
abhishekH1992 <abhishekhonrao103@gmail.com>
2024-10-20 11:05:07 +1300

    Client folder path

--- server/index.js
+++ server/index.js
@@ -94,11 +94,11 @@ async function startServer() {
 
     if (process.env.NODE_ENV === 'production') {
         // Serve static files from the React app
-        app.use(express.static(path.join(__dirname, 'client/build')));
+        app.use(express.static(path.resolve(__dirname, '../client/build')));
     
         // Handles any requests that don't match the ones above
         app.get('*', (req, res) => {
-            res.sendFile(path.join(__dirname + '/client/build/index.html'));
+            res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
         });
     }
 
