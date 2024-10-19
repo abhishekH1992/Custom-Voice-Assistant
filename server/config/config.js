9b3e7638659fb212af69eb2ec82e220a31f56447
abhishekH1992 <abhishekhonrao103@gmail.com>
2024-10-20 11:38:39 +1300

    Database url changed

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    use_env_variable: "DATABASE_URL",
+    use_env_variable: "JAWSDB_URL",
     dialect: "mysql",
     dialectModule: require('mysql2'),
     dialectOptions: {
