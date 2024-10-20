806cef7a6d92d845edbf53e9db22f55d80511967
abhishekH1992 <abhishekhonrao103@gmail.com>
2024-10-20 13:26:10 +1300

    Fixes

--- server/resolvers/conversation.resolver.js
+++ server/resolvers/conversation.resolver.js
@@ -72,7 +72,6 @@ const conversationResolver = {
                 activeStreams.set(templateId, abortController);
 
                 const combinedStreamInstance = combinedStream(stream, templateId, abortController.signal);
-                console.log(combinedStreamInstance);
                 try {
                     for await (const part of combinedStreamInstance) {
                         if (part.messageStreamed) {
